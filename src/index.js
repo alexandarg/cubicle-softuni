023dfa0476b9f3943a3a35bf1d8940a1228855e2
Alexander Georgiev <firezplay4545@gmail.com>
2022-01-27 16:17:42 +0200

    Add static asset hosting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const env = process.env.NODE_ENV || 'development';
 const config = require('./config/config')[env];
 
-const app = require('express')();
-require('./config/handlebars')(app);
+const express = require('express');
+const path = require('path');
+const initHandlebars = require('./config/handlebars');
+
+const app = express();
+initHandlebars(app);
+
+app.use(express.static(path.resolve(__dirname, './public')))
 
 app.all('/', (req, res) => {
-    res.write('It\'s working!');
-    res.end();
+    res.render('index');
 })
 
 app.listen(config.port, console.log(`Server is runing on http://localhost:${config.port}...`));
