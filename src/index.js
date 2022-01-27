a9bd37bacc2501b0cafc0491b41e13b4b2e8fb15
Alexander Georgiev <firezplay4545@gmail.com>
2022-01-27 15:57:42 +0200

    Setup handlebars

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const env = process.env.NODE_ENV || 'development';
-
 const config = require('./config/config')[env];
+
+const initHandlebars = require('./config/handlebars');
+
 const app = require('express')();
+
+initHandlebars(app);
 
 app.all('/', (req, res) => {
     res.write('It\'s working!');
