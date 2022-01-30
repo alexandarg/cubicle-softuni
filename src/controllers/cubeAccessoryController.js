ebd867a0e07f7e600a76aa860da0434a125cc28e
Alexander Georgiev <firezplay4545@gmail.com>
2022-01-30 18:35:11 +0200

    Dynamic view attach accessory page

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,7 +1,11 @@
-const router = require('express').Router({mergeParams: true});
+const router = require('express').Router({ mergeParams: true });
+
+const cubeServices = require('../services/cubeServices');
 
 router.get('/add', async (req, res) => {
-    res.render('cube/accessory/add');
+    const cube = await cubeServices.getById(req.params.cubeId);
+
+    res.render('cube/accessory/add', { ...cube });
 })
 
 module.exports = router;
