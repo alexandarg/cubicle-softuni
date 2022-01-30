af2c327a99e419c8cb6b4e4f83f70c26506eb44d
Alexander Georgiev <firezplay4545@gmail.com>
2022-01-30 18:11:42 +0200

    Create accessory
    + added button in cube details for attaching accessories

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,7 +1,26 @@
 const router = require('express').Router();
 
+const accessoryServices = require('../services/accessoryServices');
+
+const createAccessory = async (req, res) => {
+    const { name, description, imageUrl } = req.body;
+
+    try {
+        await accessoryServices.create(name, description, imageUrl);
+        
+        console.log(`${name} accessory has been successfully added to the database!`);
+
+        res.redirect('/');
+    } catch (error) {
+        res.status(400);
+        res.send(error.message);
+        res.end();
+    }
+}
+
 router.get('/create', (req, res) => {
     res.render('accessory/create');
 })
+router.post('/create', createAccessory);
 
 module.exports = router;
