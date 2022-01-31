489f48679347d9537947b70cf2cd7321815fd778
Alexander Georgiev <firezplay4545@gmail.com>
2022-01-31 12:43:01 +0200

    Show dynamic accessory select on accessory attach page

--- src/controllers/cubeAccessoryController.js
+++ src/controllers/cubeAccessoryController.js
@@ -1,11 +1,13 @@
 const router = require('express').Router({ mergeParams: true });
 
 const cubeServices = require('../services/cubeServices');
+const accessoryServices = require('../services/accessoryServices');
 
 router.get('/add', async (req, res) => {
     const cube = await cubeServices.getById(req.params.cubeId);
+    const accessories = await accessoryServices.getAll();
 
-    res.render('cube/accessory/add', { ...cube });
+    res.render('cube/accessory/add', { ...cube, accessories });
 })
 
 module.exports = router;
