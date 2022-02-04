ead0e58461321be9ded37c9cb73d71e096a40ee5
Alexander Georgiev <firezplay4545@gmail.com>
2022-02-04 19:34:44 +0200

    Added validation for create cube page and edit cube page

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -14,13 +14,16 @@ const createCube = async (req, res) => {
     const { name, description, imageUrl, difficulty } = req.body;
 
     try {
-        await cubeServices.create(name, description, imageUrl, difficulty);
+        await cubeServices.create(name, description, imageUrl, difficulty, req.user._id);
 
         console.log(`${name} with difficulty level ${difficulty} was added to the database successfully`);
 
         res.redirect('/');
     } catch (error) {
-        res.status(400).send(error.message).end();
+        const errors = Object.keys(error.errors).map(x => error.errors[x].message);
+        
+        res.locals.errors = errors;
+        res.status(400).render('cube/create');
     }
 
 }
@@ -40,10 +43,20 @@ const getEditCubePage = async (req, res) => {
 const postEditCubePage = async (req, res) => {
     const cubeId = req.params.cubeId;
     const { name, description, imageUrl, difficulty } = req.body;
+    try {
+        await cubeServices.update(cubeId, { name, description, imageUrl, difficulty });
+        
+        console.log(`${name} has been updated and saved successfully!`);
 
-    await cubeServices.update(cubeId, { name, description, imageUrl, difficulty });
+        res.redirect(`/cube/${cubeId}`);
+    } catch (error) {
+        const errors = Object.keys(error.errors).map(x => error.errors[x].message);
+        const cube = await cubeServices.getById(cubeId);
 
-    res.redirect(`/cube/${cubeId}`);
+        res.locals.errors = errors;
+        res.status(400).render('cube/edit', { ...cube });
+    }
+
 };
 
 const getDeleteCubePage = async (req, res) => {
@@ -54,6 +67,8 @@ const getDeleteCubePage = async (req, res) => {
 
 const postDeleteCubePage = async (req, res) => {
     await cubeServices.deleteById(req.params.cubeId);
+
+    console.log('Cube has been deleted from the database!');
 
     res.redirect('/');
 };
