19ade2163d347118208b52d46b32e72c25888256
Alexander Georgiev <firezplay4545@gmail.com>
2022-02-04 17:14:07 +0200

    Update authServices.js

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -2,16 +2,16 @@ const User = require('../models/User');
 const { SECRET } = require('../constants');
 const { jwtSign } = require('../utils/jwtSign');
 
-const register = (username, password, repeatPassword) => User.create({username, password, repeatPassword });
+const register = (username, password, repeatPassword) => User.create({ username, password, repeatPassword });
 
 const login = (username, password) => {
-    return User.findOne({username})
+    return User.findOne({ username })
         .then(user => Promise.all([user.validatePassword(password), user]))
         .then(([isValid, user]) => {
             if (isValid) {
                 return user;
             } else {
-                throw { message: 'Cannot find username or password'}
+                throw { message: 'Invalid username or password!' }
             }
         })
         .catch(() => null);
