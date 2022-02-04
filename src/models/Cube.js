b7622fe804f4d5387945681e6780ca72a8b6150f
Alexander Georgiev <firezplay4545@gmail.com>
2022-02-04 18:34:36 +0200

    Add cube database level validation

--- src/models/Cube.js
+++ src/models/Cube.js
@@ -3,22 +3,25 @@ const mongoose = require('mongoose');
 const cubeSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Cube name is required!'],
+        minlength: [5, 'Cube name length should be at least 5 characters long!'],
+        validate: [/^[A-Za-z0-9 ]+$/, 'Cube name should consist only of english letters digits and whitespaces!'],
     },
     description: {
         type: String,
-        required: true,
-        maxlength: 500,
+        required: [true, 'Cube description is required!'],
+        maxlength: [500, 'Cube description max lenght is 500 characters!'],
+        minlength: [20, 'Cube descripition min lenght is 20 characters!']
     },
     imageUrl: {
         type: String,
-        required: true,
-        validate: [/^https?:\/\//i, 'Entered image url is invalid!The url should start with http/https'],
+        required: [true, 'Image url is required!'],
+        validate: [/^https?:\/\//i, 'Entered image url is invalid! The url should start with http or https'],
 
     },
     difficulty: {
         type: Number,
-        required: true,
+        required: [true, 'Difficulty level is required'],
         min: 1,
         max: 6,
     },
@@ -28,6 +31,10 @@ const cubeSchema = new mongoose.Schema({
             ref: 'Accessory',
         }
     ],
+    creator: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }
 })
 
 const Cube = mongoose.model('Cube', cubeSchema);
